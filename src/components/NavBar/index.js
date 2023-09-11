352406c022677244401b1a12ae793b84b21ec625
Shelby Keene <ShelbyEKeene@gmail.com>
2023-09-10 20:03:57 -0500

    saved

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
