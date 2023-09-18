b50089062e39f8ecb1d92d413582710d6d22f2eb
Shelby Keene <ShelbyEKeene@gmail.com>
2023-09-18 12:14:58 -0500

    mongo db and firebase login and create account working, need to adress the token

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,62 +1,103 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  // createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
 
-function Login({ setToken,navigate }) {
+function Login() {
+  const firebaseConfig = {
+    apiKey: "AIzaSyBWmVGjU4SJTNC3x_WdFzdaW4i8fE9VAcM",
+    authDomain: "my-first-dcf58.firebaseapp.com",
+    databaseURL: "https://my-first-dcf58-default-rtdb.firebaseio.com",
+    projectId: "my-first-dcf58",
+    storageBucket: "my-first-dcf58.appspot.com",
+    messagingSenderId: "877230109584",
+    appId: "1:877230109584:web:bafb9925c51f1eb13ab915",
+    measurementId: "G-S101Y2H5KH",
+  };
+  
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+  const provider = new GoogleAuthProvider();
+
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
-  const [success, setSuccess] = useState('');
+  const [loggedInUser, setLoggedInUser] = useState(null);
 
-  const handleLogin = async () => {
-    try {
-      const response = await fetch('http://localhost:3000/account/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email, password }),
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setLoggedInUser(user);
+    });
+
+    return () => unsubscribe();
+  }, [auth]);
+
+  const handleLogin = () => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        console.log(error);
       });
+  };
 
-      if (response.ok) {
-        const data = await response.json();
-        
-        // Save token to local storage
-        localStorage.setItem('userToken', data.refreshToken);
-        // Storing user data in local storage after successful login
-        localStorage.setItem('user', JSON.stringify(data.user));
+  const handleGoogleLogin = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user;
 
-        // Set token in parent component
-        setToken(data.refreshToken);
-        // Display success message
-        navigate("/");
-        setSuccess('Successfully logged in');
-      } else {
-        setError('Invalid credentials');
-      }
-    } catch (error) {
-      setError('Internal Server Error');
-    }
+        if (user.email && user.email.includes("@")) {
+          console.log("Access granted.");
+          setLoggedInUser(user);
+        } else {
+          console.log("Access denied");
+          auth.signOut();
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleLogout = () => {
+    auth.signOut();
   };
 
   return (
     <div>
-      <h2>Login</h2>
-      {error && <p>{error}</p>}
-      {success && <p>{success}</p>}
-      <input
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={handleLogin}>Login</button>
+      {loggedInUser ? (
+        <div>
+          <p>You are logged in using the following email: {loggedInUser.email}</p>
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+      ) : (
+        <div>
+          <p>Please log in with your Okta Login</p>
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button onClick={handleLogin}>Login</button>
+          <button onClick={handleGoogleLogin}>Login with Google</button>
+        </div>
+      )}
     </div>
   );
 }
 
 export default Login;
+
